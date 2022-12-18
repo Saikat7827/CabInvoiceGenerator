84bf2efcbc0293ea13acce4288ac598c24203093
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-18 20:47:47 +0530

    Added : Welcome Message for Cab Invoice Generator

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Programme");
+    }
+}
