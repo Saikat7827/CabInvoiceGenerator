54e88323676469d041211054a60b01b0f45bb2c7
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-18 21:02:36 +0530

    Added : Fare Calculater For Specific Distance

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab Invoice Programme");
+    private static final int MINIMUM_COST_PER_KILOMETER = 10;
+
+    private static final int COST_PER_TIME = 1;
+
+    public static double calculateFare(double distance,int time){
+      return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
     }
+
 }
